ad62dca5794992fdd54a67bc609c300f93488ec8
John Magee <jemagee@cox.net>
2016-01-14 20:46:27 -0800

    Writing some basic scopes

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,4 +3,9 @@ class Participant < ActiveRecord::Base
   belongs_to :team
   has_many :statistics
   has_many :players, through: :statistics
+
+  scope :home, -> {where(homeaway: 'H')}
+  scope :away, -> {where(homeaway: 'A')}
+  scope :win, -> {where(winloss: 'W')}
+  scope :loss, -> {where(winloss: 'L')}
 end
