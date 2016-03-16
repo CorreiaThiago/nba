76ec504714de56aae1137856ecd570a6794b9ffb
John Magee <jemagee@cox.net>
2016-03-15 18:59:58 -0700

    Moving first participant test from the game model spec file to the participant model spec file

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 RSpec.describe Participant, type: :model do
   it {should belong_to(:team)}
   it {should belong_to(:game)}
+
+  let(:test_case) { JSON.parse(File.read('spec/json_tests/games_test.json'))["resultSets"]}
+
+  scenario "Adding the participants" do
+  	expect {Game.add_games(test_case) }.to change{ Participant.count }.by(10)
+  end
 end
+
