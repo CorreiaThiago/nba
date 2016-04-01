86358b74fdf5d436265263b842aa4b32f174913d
John Magee <jemagee@cox.net>
2016-03-31 19:55:39 -0700

    Beginning of testing Player model works

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Player, type: :model do
   it {should validate_presence_of(:height)}
   it {should validate_numericality_of(:height).only_integer}
   it {should validate_numericality_of(:rookie_year).only_integer}
+
+  let(:playerinfo) { JSON.parse(File.read('spec/json_tests/player_test.json'))}
+
+  scenario "Assuming you get the right JSON the player is added properly" do
+    Player.stub(:exists?).and_return(false)
+    Player.stub(:get_player).and_return(JSON.parse(File.read('spec/json_tests/player_test.json'))["resultSets"][0]["rowSet"][0])
+    expect{Player.checkplayer(2404)}.to change{Player.count}.by(1)
+  end
 end
