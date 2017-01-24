0efeaf3955583e10603dd0113c45e8a843b99872
John Magee <jemagee@cox.net>
2017-01-24 13:16:48 -0800

    Integrating VCR and Webmock, testing on Games

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,3 +69,9 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
+end
