bf93b8c62b679be1fa14f32ce3ee8042f12382f5
John Magee <jemagee@cox.net>
2017-05-01 19:46:46 -0700

    Reformatting Standings tests to pass after changing partials to present data differently

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe TeamsController, type: :controller do
 
-  describe "GET #standings" do
-    it "returns http success" do
-      get :standings
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
--- spec/features/standings_spec.rb
+++ spec/features/standings_spec.rb
@@ -25,27 +25,27 @@ RSpec.feature "Testing the Standings page" do
 
 	scenario "The eastern conference divisions are listed in order" do
 		within("div#standings div[data-conference='eastern']") do
-			within('div[data-division]:nth-child(1)') do
+			within('div[data-division]:nth-of-type(1)') do
 				expect(page).to have_content("Atlantic Division")
 			end
-			within('div[data-division]:nth-child(2)') do
+			within('div[data-division]:nth-of-type(2)') do
 				expect(page).to have_content("Central Division")
 			end
-			within('div[data-division]:nth-child(3)') do
+			within('div[data-division]:nth-of-type(3)') do
 				expect(page).to have_content("Southeast Division")
 			end
 		end
-	end
+	end 
 
 	scenario "The western conference divisions are listed in order" do
 		within("div#standings div[data-conference='western']") do
-			within('div[data-division]:nth-child(1)') do
+			within('div[data-division]:nth-of-type(1)') do
 				expect(page).to have_content("Northwest Division")
 			end
-			within('div[data-division]:nth-child(2)') do
+			within('div[data-division]:nth-of-type(2)') do
 				expect(page).to have_content("Pacific Division")
 			end
-			within('div[data-division]:nth-child(3)') do
+			within('div[data-division]:nth-of-type(3)') do
 				expect(page).to have_content("Southwest Division")
 			end
 		end
@@ -59,13 +59,13 @@ RSpec.feature "Testing the Standings page" do
 
 
 	scenario "Expect the first place team in the Atlantic Division to be the Toronto Raptors" do
-		within("div[data-division='Atlantic'] div[data-place='0']") do
+		within("div#atlantic-0") do
 			expect(page).to have_content("Toronto Raptors")
 		end
 	end
 
 	scenario "Expec the third place team in the Central Division to have be the Chicago Bulls" do
-		within ("div[data-division='Central'] div[data-place='2']") do
+		within ("div#central-2") do
 			expect(page).to have_content("Chicago Bulls")
 		end
 	end
